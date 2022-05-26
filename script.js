8d1cf3dce05f140fa2635038e6a3258269016605
Mehmet-Durna <r0900513@student.thomasmore.be>
2022-05-26 22:07:12 +0200

    initialize

---
+++ script.js
@@ -0,0 +1,10 @@
+const years = document.getElementById("years");
+const months = document.getElementById("months");
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+const loading = document.getElementById("loading");
+
+

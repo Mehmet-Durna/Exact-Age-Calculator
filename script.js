52a69d51294516eccc5592a61bdc78846480dd44
Mehmet-Durna <r0900513@student.thomasmore.be>
2022-05-28 23:18:44 +0200

    h2 elements caught

--- script.js
+++ script.js
@@ -4,12 +4,20 @@ const days = document.getElementById("days");
 const hours = document.getElementById("hours");
 const minutes = document.getElementById("minutes");
 const seconds = document.getElementById("seconds");
-
+const countdown = document.getElementById("countdown")
 const loading = document.getElementById("loading");
 
 window.addEventListener("load",()=>{
    loading.style.display = "block";
    setTimeout(()=>{
        loading.style.display = "none";
+       countdown.style.display = "flex";
+
    },1000)
 });
+
+let h2Elements = countdown.querySelectorAll("h2");
+
+h2Elements.forEach(element=>{
+    element.innerHTML = "00";
+})
